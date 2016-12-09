c746c0886ddd65f31c799fa56881387869517eed
BrianUrielRomero <pizkoo41@gmail.com>
2016-12-08 19:44:40 -0500

    money

--- aplication/database.php
+++ aplication/database.php
@@ -16,7 +16,7 @@ class classPDO
 	public function __construct(
 		$drive = "mysql",
 		$host = "localhost",
-		$database = "test",
+		$database = "money",
 		$username = "root",
 		$password = ""
 	){
--- views/users/index.php
+++ views/users/index.php
@@ -1,27 +1,48 @@
+<div class="row">
+	<div class="col-xs-12">
+		
+	</div>
+</div>
 <h2>Usuarios</h2>
+<div class="table-responsive">
+	<table class="table table-striped"> 
+		<tr>
+			<th>ID</th>
+			<th>username</th>
+			<th>Password</th>
+			<th>Type</th>
+			<th>Action</th>
+		</tr>
 
-
-<table>
+	<?php foreach ($users as $user): ?>
 	<tr>
-		<th>ID</th>
-		<th>username</th>
-		<th>Password</th>
-		<th>Type</th>
-		<th>Action</th>
+		<td><?php echo $user["users"]["id"]; ?></td>
+		<td><?php echo $user["users"]["username"]; ?></td>
+		<td><?php echo $user["users"]["password"]; ?></td>
+		<td><?php echo $user["types"]["name"]; ?></td>
+		<td>
+
+			<?php
+				echo $this->Html->link(
+					"<span class=\"glyphicon glyphicon-pencil\" aria-hidden=\"true\"></span>", "/users/edit/".$user["users"]["id"],
+					array(
+						"title"=>"Editar usuario",
+						"target"=>"_blank"
+					)
+				);
+			?>
+			<?php
+				echo $this->Html->link(
+					"Delete", 
+					"/users/delete/".$user["users"]["id"]
+					
+				);
+			?>
+			
+		</td>
 	</tr>
+	<?php endforeach; ?>
 
-<?php foreach ($users as $user): ?>
-<tr>
-	<td><?php echo $user["users"]["id"]; ?></td>
-	<td><?php echo $user["users"]["username"]; ?></td>
-	<td><?php echo $user["users"]["password"]; ?></td>
-	<td><?php echo $user["types"]["name"]; ?></td>
-	<td>
-		<a href="<?php echo APP_URL."/users/edit/".$user["users"]["id"]; ?>">Edit</a>
-		<a href="<?php echo APP_URL."/users/delete/".$user["users"]["id"]; ?>">Delete</a>
-
-	</td>
-</tr>
-<?php endforeach; ?>
-
-</table>
+	</table>
+	<a href="<?php echo APP_URL; ?>/users/add" ><button  type="button" class="btn btn-primary" >Crear Usuarios</button></a>
+</div>
